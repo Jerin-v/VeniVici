52caef0f39f4ecca0609ad91a4fedfe67c50d603
Jerin Vandannoor <108553514+Jerin-v@users.noreply.github.com.>
2024-03-20 13:34:21 -0500

    added basic page

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,22 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-      
+    <div className='container'>
+      <div className='mainPage'>
+        <h1>Museum</h1>
+        <h3>discover art pieces from the Harvard Art Museum!</h3>
+        <br></br>
+        <br></br>
+        <button className='discover'>Discover</button>
+      </div>
+
+      <div className='sideBar'>
+        <div className='banList'>
+          <h3>Ban List</h3>
+
+        </div>
+
+      </div>
     </div>
   )
 }
