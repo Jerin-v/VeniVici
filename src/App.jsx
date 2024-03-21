fb5ad3419664e04e6951db79ce08aade1ad1b194
Jerin Vandannoor <108553514+Jerin-v@users.noreply.github.com.>
2024-03-21 17:30:16 -0500

    removed call api function and added component

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Discover from './discover'
 
 function App() {
   const [count, setCount] = useState(0)
 
-  async function callAPI() {
-
-    try {
-    const response = await fetch("https://api.harvardartmuseums.org/object?apikey=727f9bec-11ef-4988-ae72-f65ab031a8f1&size=50")
-    if(!response.ok) {
-      throw new Error("Could not fetch resource")
-    }
-    const data = await response.json()
-    console.log(data)
-    }
-    catch (error){
-      console.error(error)
-    }
-  }
-  callAPI()
-
   return (
     <div className='container'>
       <div className='mainPage'>
-        <h1>Museum</h1>
-        <h3>discover art pieces from the Harvard Art Museum!</h3>
+        <h1>Trippin' On Cats</h1>
+        <h3>discover cats from your wildest dreams!</h3>
         <br></br>
         <br></br>
-        <button className='discover'>Discover</button>
+        <div className='discoverContainer'>
+          <Discover />
+          <button className='discoverButton'>Discover</button>
+        </div>
         
       </div>
 
