fa8e4ef1e7927f65e653c70afc29e070ee7929ea
Jerin Vandannoor <108553514+Jerin-v@users.noreply.github.com.>
2024-03-21 19:03:22 -0500

    added useState to display elements on screen

--- src/discover.jsx
+++ src/discover.jsx
@@ -1,27 +1,39 @@
-
+import { useEffect, useState } from "react"
 
 const Discover = () => {
 
-    async function callAPI() {
-        const response = await fetch("https://api.thecatapi.com/v1/images/search?has_breeds=1&api_key=live_S0CaHgx2ccgXfVQILiaXBFMsb7l84mEUDiK3aBsve6BhyVB8D5Thgcak5xmrcBWj")
-        const data = await response.json()
-        console.log(data)
-    
-    
-    
-    }
-    callAPI()
+    const [name, setName] = useState('')
+    const [origin, setOrigin] = useState('')
+    const [weight, setWeight] = useState('')
+    const [life, setLife] = useState('')
+    const [img,setImg] = useState('')
+
+    useEffect(() => {
+        async function callAPI() {
+            const response = await fetch("https://api.thecatapi.com/v1/images/search?has_breeds=1&api_key=live_S0CaHgx2ccgXfVQILiaXBFMsb7l84mEUDiK3aBsve6BhyVB8D5Thgcak5xmrcBWj")
+            const data = await response.json()
+            console.log(data[0].url)
+            setName(data[0].breeds[0].name)
+            setOrigin(data[0].breeds[0].origin)
+            setWeight(data[0].breeds[0].weight.imperial)
+            setLife(data[0].breeds[0].life_span)
+            setImg(data[0].url)
+        }
+
+        callAPI()
+
+    },[])
 
     return (
         <div>
             <h2>Some Cat Name</h2>
             <div className="buttons">
-                <button type="attribute" className="attribute-button"></button>
-                <button type="attribute" className="attribute-button"></button>
-                <button type="attribute" className="attribute-button"></button>
-                <button type="attribute" className="attribute-button"></button>
+                <button className="attribute">{ name }</button>
+                <button className="attribute">{ origin }</button>
+                <button className="attribute">{ weight } Pounds</button>
+                <button className="attribute">{ life } Years</button>  
             </div>
-            <img className="carPicture" src="" alt="random cat picture"></img>
+            <img className="carPicture" src={img} alt="random cat picture" height="250px" width="250px" ></img>
         </div>
     )
 }
