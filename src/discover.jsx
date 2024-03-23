c6cf1a4e1c9733dda3ebd043ab1e6a40512e347b
Jerin Vandannoor <108553514+Jerin-v@users.noreply.github.com.>
2024-03-22 22:43:23 -0500

    removed banList array

--- src/discover.jsx
+++ src/discover.jsx
@@ -8,28 +8,29 @@ const Discover = () => {
     const [life, setLife] = useState('')
     const [img,setImg] = useState('')
 
+
     useEffect(() => {
         async function callAPI() {
             const response = await fetch("https://api.thecatapi.com/v1/images/search?has_breeds=1&api_key=live_S0CaHgx2ccgXfVQILiaXBFMsb7l84mEUDiK3aBsve6BhyVB8D5Thgcak5xmrcBWj")
             const data = await response.json()
-            console.log(data[0].url)
             setName(data[0].breeds[0].name)
             setOrigin(data[0].breeds[0].origin)
             setWeight(data[0].breeds[0].weight.imperial)
             setLife(data[0].breeds[0].life_span)
             setImg(data[0].url)
+            console.log(data[0])
         }
-
         callAPI()
 
     },[])
+
 
     return (
         <div>
             <h2>Some Cat Name</h2>
             <div className="buttons">
-                <button className="attribute">{ name }</button>
-                <button className="attribute">{ origin }</button>
+                <button  className="attribute">{ name }</button>
+                <button  className="attribute">{ origin }</button>
                 <button className="attribute">{ weight } Pounds</button>
                 <button className="attribute">{ life } Years</button>  
             </div>
